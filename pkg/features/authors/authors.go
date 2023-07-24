ccf3cde8200026a189a123b04ecdbe3e873e56db
Kevin C. Krinke <kevin@krinke.ca>
2023-07-24 14:22:56 -0400

    pkg/features/authors: migrate to xsync.Map structures; code cosmetic changes

--- pkg/features/authors/authors.go
+++ pkg/features/authors/authors.go
@@ -22,6 +22,7 @@ import (
 	"strings"
 
 	"github.com/maruel/natural"
+	"github.com/puzpuzpuz/xsync/v2"
 	"github.com/urfave/cli/v2"
 
 	"github.com/go-enjin/golang-org-x-text/language"
@@ -34,6 +35,7 @@ import (
 	"github.com/go-enjin/be/pkg/page"
 	"github.com/go-enjin/be/pkg/request/argv"
 	beStrings "github.com/go-enjin/be/pkg/strings"
+
 	"github.com/go-enjin/website-quoted-fyi/pkg/quote"
 )
 
@@ -56,9 +58,9 @@ type CFeature struct {
 
 	numQuotes int
 
-	authorNameByAuthorKey map[string]string
-	authorKeyByAuthorName map[string]string
-	authorNamesByLetter   map[string][]string
+	authorNameByAuthorKey *xsync.MapOf[string, string]
+	authorKeyByAuthorName *xsync.MapOf[string, string]
+	authorNamesByLetter   *xsync.MapOf[string, []string]
 }
 
 type MakeFeature interface {
@@ -73,9 +75,9 @@ func NewTagged(tag feature.Tag) MakeFeature {
 	f := new(CFeature)
 	f.Init(f)
 	f.FeatureTag = tag
-	f.authorNameByAuthorKey = make(map[string]string)
-	f.authorKeyByAuthorName = make(map[string]string)
-	f.authorNamesByLetter = make(map[string][]string)
+	f.authorNameByAuthorKey = xsync.NewMapOf[string]()
+	f.authorKeyByAuthorName = xsync.NewMapOf[string]()
+	f.authorNamesByLetter = xsync.NewMapOf[[]string]()
 	return f
 }
 
@@ -134,16 +136,18 @@ func (f *CFeature) AddToIndex(stub *fs.PageStub, p *page.Page) (err error) {
 		return
 	}
 
-	if _, present := f.authorNameByAuthorKey[authorKey]; present {
+	if _, present := f.authorNameByAuthorKey.Load(authorKey); present {
 		return
 	}
 
-	f.authorNameByAuthorKey[authorKey] = authorName
-	f.authorKeyByAuthorName[authorName] = authorKey
+	f.authorNameByAuthorKey.Store(authorKey, authorName)
+	f.authorKeyByAuthorName.Store(authorName, authorKey)
 
 	if lastName := beStrings.LastName(authorName); lastName != "" {
 		fc := strings.ToLower(string(lastName[0]))
-		f.authorNamesByLetter[fc] = append(f.authorNamesByLetter[fc], authorName)
+		names, _ := f.authorNamesByLetter.Load(fc)
+		names = append(names, authorName)
+		f.authorNamesByLetter.Store(fc, names)
 	}
 
 	return
@@ -169,18 +173,9 @@ func (f *CFeature) ProcessPagePath(w http.ResponseWriter, r *http.Request) (proc
 		authorKey := strings.ToLower(m[0][1])
 		// log.WarnF("hit author page: %v", authorKey)
 
-		//selectedAuthorName := f.Enjin.SelectQL(fmt.Sprintf(`SELECT DISTINCT .QuoteAuthor WITHIN (.QuoteAuthorKey == "%v")`, authorKey))
-		//var authorName string
-		//if name, ok := selectedAuthorName["QuoteAuthor"].(string); ok {
-		//	authorName = name
-		//} else {
-		//	log.ErrorF("author not found by key: %v - %#+v", authorKey, selectedAuthorName)
-		//	return
-		//}
-
 		var ok bool
 		var authorName string
-		if authorName, ok = f.authorNameByAuthorKey[authorKey]; !ok {
+		if authorName, ok = f.authorNameByAuthorKey.Load(authorKey); !ok {
 			return
 		}
 
@@ -271,34 +266,16 @@ func (f *CFeature) ProcessGroupPath(w http.ResponseWriter, r *http.Request) (pro
 		m := RxGroupPath.FindAllStringSubmatch(r.URL.Path, 1)
 		groupChar := strings.ToLower(m[0][1])
 		// log.WarnF("hit authors group: %v", groupChar)
-		//results := f.Enjin.SelectQL(`SELECT DISTINCT .QuoteAuthor`)
-		// log.WarnF("results: %#v", results)
 
-		//var authors, authorLetters []string
-		//var totalNumAuthors int
-		//if present, ok := results["QuoteAuthor"].([]interface{}); ok {
-		//	// log.WarnF("num authors: %v", len(present))
-		//	for _, thing := range present {
-		//		if fullName, ok := thing.(string); ok {
-		//			totalNumAuthors += 1
-		//			if lastName := beStrings.LastName(fullName); lastName != "" {
-		//				fc := strings.ToLower(string(lastName[0]))
-		//				if !beStrings.StringInSlices(fc, authorLetters) {
-		//					authorLetters = append(authorLetters, fc)
-		//				}
-		//				if fc == groupChar {
-		//					authors = append(authors, fullName)
-		//				}
-		//			}
-		//		}
-		//	}
-		//}
-		//sort.Sort(natural.StringSlice(authors))
-		//sort.Sort(natural.StringSlice(authorLetters))
+		namesByLetter, _ := f.authorNamesByLetter.Load(groupChar)
+		authors := beStrings.SortedByLastName(namesByLetter)
 
-		authors := beStrings.SortedByLastName(f.authorNamesByLetter[groupChar])
-
-		authorLetters := maps.SortedKeys(f.authorNamesByLetter)
+		var authorLetters []string
+		//authorLetters := maps.SortedKeys(f.authorNamesByLetter)
+		f.authorNamesByLetter.Range(func(letter string, _ []string) bool {
+			authorLetters = append(authorLetters, letter)
+			return true
+		})
 		sort.Sort(natural.StringSlice(authorLetters))
 
 		if listingPage := f.Enjin.FindPage(f.Enjin.SiteDefaultLanguage(), "!authors-key"); listingPage != nil {
@@ -307,7 +284,7 @@ func (f *CFeature) ProcessGroupPath(w http.ResponseWriter, r *http.Request) (pro
 			listingPage.Context.SetSpecific("AuthorLetters", authorLetters)
 			listingPage.Context.SetSpecific("NumAuthors", len(authors))
 			listingPage.Context.SetSpecific("AuthorCharacter", groupChar)
-			listingPage.Context.SetSpecific("TotalNumAuthors", len(f.authorNameByAuthorKey))
+			listingPage.Context.SetSpecific("TotalNumAuthors", f.authorNameByAuthorKey.Size())
 			if err := f.Enjin.ServePage(listingPage, w, r); err != nil {
 				log.ErrorF("error serving authors listing page: %v", err)
 			} else {
@@ -332,7 +309,7 @@ func (f *CFeature) ProcessRequestPageType(r *http.Request, p *page.Page) (pg *pa
 		//pg = p
 		//processed = true
 		p.Context.SetSpecific("NumQuotes", f.numQuotes)
-		p.Context.SetSpecific("NumAuthors", len(f.authorKeyByAuthorName))
+		p.Context.SetSpecific("NumAuthors", f.authorKeyByAuthorName.Size())
 	}
 
 	return
@@ -341,22 +318,22 @@ func (f *CFeature) ProcessRequestPageType(r *http.Request, p *page.Page) (pg *pa
 func (f *CFeature) ProcessSinglePageType(r *http.Request, p *page.Page) (pg *page.Page, redirect string, processed bool, err error) {
 
 	// log.WarnF("hit author page type: %v", p.Url)
-	// quoteGroups := make([]*quote.QuotesGroup, 0)
-	// quoteLookup := make(map[string][]*quote.Quote)
 
 	return
 }
 
 func (f *CFeature) ProcessGroupsPageType(r *http.Request, p *page.Page) (pg *page.Page, redirect string, processed bool, err error) {
 	authorGroups := make([]*quote.AuthorsGroup, 0)
-	//authorLookup := make(map[string][]*quote.Quote)
 
 	authors := p.Context.Strings("Authors")
 	if len(authors) == 0 {
-		//authors = maps.SortedKeysByLastName(f.authorKeyByAuthorName)
-		//p.Context.SetSpecific("Authors", []interface{}{authors})
-		p.Context.SetSpecific("NumAuthors", len(f.authorKeyByAuthorName))
-		authorLetters := maps.SortedKeys(f.authorNamesByLetter)
+		p.Context.SetSpecific("NumAuthors", f.authorKeyByAuthorName.Size())
+		var authorLetters []string
+		f.authorNamesByLetter.Range(func(letter string, _ []string) bool {
+			authorLetters = append(authorLetters, letter)
+			return true
+		})
+		sort.Sort(natural.StringSlice(authorLetters))
 		p.Context.SetSpecific("AuthorLetters", authorLetters)
 		//pg = p
 		//processed = true
@@ -366,18 +343,19 @@ func (f *CFeature) ProcessGroupsPageType(r *http.Request, p *page.Page) (pg *pag
 	var groupIdx int
 	var groupKey string
 	for _, authorName := range authors {
-		authorKey := quote.GetLastNameKey(authorName)
-		if len(authorGroups) == 0 || groupKey != authorKey {
+		lastNameKey := quote.GetLastNameKey(authorName)
+		if len(authorGroups) == 0 || groupKey != lastNameKey {
 			authorGroups = append(authorGroups, &quote.AuthorsGroup{
-				Key: authorKey,
+				Key: lastNameKey,
 			})
 			groupIdx = len(authorGroups) - 1
-			groupKey = authorKey
+			groupKey = lastNameKey
 		}
+		authorKey, _ := f.authorKeyByAuthorName.Load(authorName)
 		authorGroups[groupIdx].Authors = append(
 			authorGroups[groupIdx].Authors,
 			&quote.Author{
-				Key:  f.authorKeyByAuthorName[authorName],
+				Key:  authorKey,
 				Name: authorName,
 			},
 		)
